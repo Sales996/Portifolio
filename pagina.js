4c09b184c9415a8783994e3df060f75e0702a252
Sales996 <gabriel.boas@aluno.senai.br>
2025-03-21 21:21:51 -0300

    Update pagina.js

--- pagina.js
+++ pagina.js
@@ -1,9 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Página carregada com sucesso!');
 
-  const links = document.querySelectorAll('nav a');
-  links.forEach(link => {
-      link.addEventListener('mouseover', () => link.style.color = '#FFD700');
-      link.addEventListener('mouseout', () => link.style.color = '#fff');
-  });
-});
